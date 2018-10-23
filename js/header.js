db3e047e6858bd708d583b1082848a42ba4fb336
adffewr <adffewr@nanuly.kr>
2018-10-23 18:28:07 +0900

    나누리 홈페이지 3차 수정반영

--- js/header.js
+++ js/header.js
@@ -1,27 +1,67 @@
 $(function(){
 
 	$(".gnb > ul > li").mouseenter(function(){
-		$(".gnb_curtain").stop().slideDown()
-		$(".gnb_ul_box > li > ul").stop().slideDown()
+		$(this).children("ul").stop().fadeIn(200);
 	})
 
 	$(".gnb > ul > li").mouseleave(function(){
-		$(".gnb_curtain").stop().slideUp()
-		$(".gnb_ul_box > li > ul").stop().slideUp()
+		$(this).children("ul").stop().fadeOut(200);
 	})
 
-    $(".menu_btn_box").click(function(){ 
-        $(this).toggleClass("active") 
-        $(".snb").toggleClass("active")
+
+    $(window).scroll(function(){
+    	if (st > 0) {
+    		$("header").addClass("active")
+    	}else{
+    		$("header").removeClass("active")
+    	}
     })
 
 
-    $(".snb > ul > li").click(function(){
-    	$(".snb_box_ul").stop().slideUp();
-    	$(this).children().stop().slideDown();
 
-    })
+    var gnbNav = new Array;
+    gnbNav[0] = "/";
+    var gnburl = location.pathname.substring(1) ;
+    var getList0 = gnburl.indexOf(gnbNav[0]);
+
+    if(getList0 != -1){
+        $(".gnb > ul > li:eq(0) > a").attr("href", "index.html")
+        $(".gnb > ul > li:eq(1) > a").attr("href", "history.html")
+        $(".gnb > ul > li:eq(1) > ul > li:eq(0) > a").attr("href", "history.html")
+    };
+    
+
+
+
 
     
+    var subPage = new Array;
+    subPage[0] = "index";
+    subPage[1] = "history";
+    subPage[2] = "";
+    var url = location.href;
+    var getAr0 = url.indexOf(subPage[0]);
+    var getAr1 = url.indexOf(subPage[1]);
+    var getAr2 = url.indexOf(subPage[2]);
+
+    if(getAr0 != -1){
+        $(".gnb > ul > li:eq(0) > a").parent().addClass("active")
+    };
+
+    if(getAr1 != -1){
+        $(".gnb > ul > li:eq(1) > a").parent().addClass("active")
+        $(".gnb > ul > li > ul > li:eq(0) > a").parent().addClass("active")
+        $(".breadcrumbs h1").text("History")
+        $(".breadcrumbs_ul > li > a").eq(1).text("회사소개")
+        $(".breadcrumbs_ul > li > a").eq(2).text("연혁")
+    };
+
+    // if(getAr2 != -1){
+    //     $("li:eq(3) a").addClass("on")
+    // };
+
+
+
+   
 
 });
--- js/home.js
+++ js/home.js
@@ -1,52 +1,134 @@
 $(function(){
 
-   n = 1
 
-   function slider_next(){
-   		$(".home_slide1").stop().animate({"left":(n-1)*-100+"%"});
-      $(".slide_btn1_txt").removeClass("active");
-      $(".slide_btn1_txt"+n).addClass("active");
-   }
+  n = 1
 
-   ClearID = setInterval(function(){ 
-        n++
-        if(n > 2){ n = 1 }
-        slider_next()
-    },5000)
-
-   $(".slide_btn1 button").mouseenter(function(){
-      clearInterval(ClearID)
-   }).mouseleave(function(){
-      ClearID = setInterval(function(){ 
-        n++
-        if(n > 2){ n = 1 }
-        slider_next()
-      },5000) 
-   })
+  ClearID = setInterval(function(){ 
+    n++
+    next() 
+  },4000)
 
 
-   clickble = true
+  setTimeout(function(){ $(".slide_img1").addClass("active")  },100)
 
-   $(".btn_next").click(function(){
-      if (clickble == true) {
-        n++
-        if(n > 2){ n = 1 }
-        slider_next()
-        clickble = false
-        setTimeout(function(){ clickble = true },1000)
-      }
+  function next(){
+    if( n== 4){ n = 1}
+    $(".slide_box li").stop().fadeOut()
+    $(".slide_img"+n).stop().fadeIn()
+
+    $(".slide_box li").removeClass("active")
+    $(".slide_img"+n).addClass("active")
+  }
+
+
+  
+
+  $(".slide_ani_btn").mouseenter(function(){
+    clearInterval(ClearID)
+  }).mouseleave(function(){
+    ClearID = setInterval(function(){ 
+      n++
+      next() 
+    },4000)
+  })
+
+
+  clickble = true
+
+  $(".slide_left_bnt").click(function(){
+    if ( clickble == true) {
+      n--
+      if ( n < 1){ n = 3}
+      $(".slide_box li").stop().fadeOut()
+      $(".slide_img"+n).stop().fadeIn()
+      clickble = false
+      setTimeout(function(){ clickble = true },1000)
+    };
+  })
+
+
+
+  $(".slide_right_bnt").click(function(){
+    if ( clickble == true ){
+      n++
+      next()
+      clickble = false
+      setTimeout(function(){ clickble = true },1000)
+    }
+    
+  })
+
+
+  $(".home_about_li").click(function(){
+    $(this).stop().toggleClass("active")
+    $(this).next().stop().slideToggle()
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // n = 1
+
+   // function slider_next(){
+   // 		$(".home_slide1").stop().animate({"left":(n-1)*-100+"%"});
+   //    $(".slide_btn1_txt").removeClass("active");
+   //    $(".slide_btn1_txt"+n).addClass("active");
+   // }
+
+   // ClearID = setInterval(function(){ 
+   //      n++
+   //      if(n > 2){ n = 1 }
+   //      slider_next()
+   //  },5000)
+
+   // $(".slide_btn1 button").mouseenter(function(){
+   //    clearInterval(ClearID)
+   // }).mouseleave(function(){
+   //    ClearID = setInterval(function(){ 
+   //      n++
+   //      if(n > 2){ n = 1 }
+   //      slider_next()
+   //    },5000) 
+   // })
+
+
+   // clickble = true
+
+   // $(".btn_next").click(function(){
+   //    if (clickble == true) {
+   //      n++
+   //      if(n > 2){ n = 1 }
+   //      slider_next()
+   //      clickble = false
+   //      setTimeout(function(){ clickble = true },1000)
+   //    }
    		
-   })
+   // })
 
-   $(".btn_prve").click(function(){
-      if (clickble == true) {
-        n--
-        if(n < 1){ n = 2 }
-        slider_next()
-        clickble = false
-        setTimeout(function(){ clickble = true },1000)
-      }
-   })
+   // $(".btn_prve").click(function(){
+   //    if (clickble == true) {
+   //      n--
+   //      if(n < 1){ n = 2 }
+   //      slider_next()
+   //      clickble = false
+   //      setTimeout(function(){ clickble = true },1000)
+   //    }
+   // })
 
 
 })
